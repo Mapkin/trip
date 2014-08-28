5b7cfeb05af74a3cae283cc7002682e7e591911d
Jacob Wasserman <jwasserman@gmail.com>
2014-08-27 22:06:06 -0400

    Initial commit of server + files
    Commit server.py, Procfile and requirements.txt

---
+++ server.py
@@ -0,0 +1,18 @@
+import os
+
+from flask import Flask, g
+import hipchat
+app = Flask(__name__)
+
+@app.route('/<room_id>')
+def callback(room_id):
+    if not hasattr(g, 'hipchatcli'):
+        g.hipchatcli = hipchat.HipChat(os.environ['HIPCHAT_TOKEN'])
+
+    sender = os.environ['HIPCHAT_SENDER']
+    g.hipchatcli.message_room(room_id, sender, 'test')
+    return 'success'
+
+
+if __name__ == '__main__':
+    app.run()
